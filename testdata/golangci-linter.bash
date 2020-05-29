cd588fc838249b0a24073754d8b03e5bd0c26e27
aerth <aerth@riseup.net>
2020-05-29 19:23:48 +0000

    tests: add golangci-linter, simplify travis.yml, workaround for <go1.11

---
+++ testdata/golangci-linter.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+golangci_linter_version=v1.27.0
+
+# fetch golangci-lint program
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin ${golangci_linter_version}
+
+# run linters
+GOOS=windows $(go env GOPATH)/bin/golangci-lint run --no-config --enable-all \
+  --color always \
+  --disable gochecknoglobals,gomnd \
+  -v \
+  ./...
+
--- testdata/travis.bash
+++ testdata/travis.bash
@@ -1,6 +1,26 @@
+#!/bin/bash
 set -ex
+
+# workaround for go1.10's no module support, we copy this run's source code into namecoin/certinject
+if [ "$TRAVIS_REPO_SLUG" != "namecoin/certinject" ] && [ "$TRAVIS_GO_VERSION" = "1.10.x" ]; then
+  mkdir $GOPATH/src/github.com/namecoin && \
+  cp -av . $GOPATH/src/github.com/namecoin/certinject && \
+  cd $GOPATH/src/github.com/namecoin/certinject
+fi
+
+echo Fetching dependencies
 go get -v -u -t ./...
+
+echo Building certinject.exe
 go build -o certinject.exe ./cmd/certinject
+
+if [ "$TRAVIS_OS_NAME" = "windows" ]; then
+echo Running powershell tests
 powershell -ExecutionPolicy Unrestricted -File "testdata/ci-failtest.ps1"
+fi
+
+echo Testing config parsing
 ./certinject.exe -conf testdata/test.conf
+
+echo Running go test suite
 go test -v ./...
