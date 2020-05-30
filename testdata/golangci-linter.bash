13b62d35f55fe30f0537c471798dc48342a38a54
aerth <aerth@riseup.net>
2020-05-30 06:24:07 +0000

    travis.yml: add linter stage for --new-from-rev=HEAD~

--- testdata/golangci-linter.bash
+++ testdata/golangci-linter.bash
@@ -12,13 +12,13 @@ echo ----- Windows -----
 GOOS=windows $(go env GOPATH)/bin/golangci-lint run --no-config --enable-all \
   --color always \
   --disable gochecknoglobals,gomnd \
-  -v \
+  -v $@ \
   ./...
 
 echo ----- Linux -----
 GOOS=linux $(go env GOPATH)/bin/golangci-lint run --no-config --enable-all \
   --color always \
   --disable gochecknoglobals,gomnd \
-  -v \
+  -v $@ \
   ./...
 
