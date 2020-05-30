5afdcdc9dac1d632e5c779873cd0761b3c267d5a
aerth <aerth@riseup.net>
2020-05-30 06:20:11 +0000

    linter: run GOOS=windows and GOOS=linux golangci-linter

--- testdata/golangci-linter.bash
+++ testdata/golangci-linter.bash
@@ -3,10 +3,20 @@
 golangci_linter_version=v1.27.0
 
 # fetch golangci-lint program
+
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin ${golangci_linter_version}
 
 # run linters
+
+echo ----- Windows -----
 GOOS=windows $(go env GOPATH)/bin/golangci-lint run --no-config --enable-all \
+  --color always \
+  --disable gochecknoglobals,gomnd \
+  -v \
+  ./...
+
+echo ----- Linux -----
+GOOS=linux $(go env GOPATH)/bin/golangci-lint run --no-config --enable-all \
   --color always \
   --disable gochecknoglobals,gomnd \
   -v \
