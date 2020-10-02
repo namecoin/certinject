7a94aa9fe3614f3fa95d8489654ed543ded4db73
Jeremy Rand <jeremyrand@airmail.cc>
2020-10-02 11:02:41 +0000

    Travis: Fix shellcheck issue

--- testdata/golangci-linter.bash
+++ testdata/golangci-linter.bash
@@ -7,7 +7,7 @@ golangci_linter_version=v1.27.0
 
 # fetch golangci-lint program
 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin ${golangci_linter_version}
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin ${golangci_linter_version}
 
 # run linters
 
@@ -23,14 +23,14 @@ do
 done
 
 echo ----- Windows -----
-GOOS=windows $(go env GOPATH)/bin/golangci-lint run --no-config --enable-all \
+GOOS=windows "$(go env GOPATH)"/bin/golangci-lint run --no-config --enable-all \
   --color always \
   --disable gochecknoglobals,gomnd \
   -v $@ \
   ./...
 
 echo ----- Linux -----
-GOOS=linux $(go env GOPATH)/bin/golangci-lint run --no-config --enable-all \
+GOOS=linux "$(go env GOPATH)"/bin/golangci-lint run --no-config --enable-all \
   --color always \
   --disable gochecknoglobals,gomnd \
   -v $@ \
