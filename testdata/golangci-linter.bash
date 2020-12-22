e637f5eb2f51fbd69694e53baee1b97b2a0f2448
Jeremy Rand <jeremyrand@airmail.cc>
2020-12-22 12:27:54 +0000

    Cirrus: Add ShellCheck task

--- testdata/golangci-linter.bash
+++ testdata/golangci-linter.bash
@@ -13,14 +13,7 @@ curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/insta
 
 echo ----- Shell -----
 
-# Run shellcheck on all shell scripts
-# The .travis directory is currently unused (relic from ncdns).  It will be
-# re-introduced later.  For now, we don't try to lint it, since it's inactive.
-ALL_SHELL="$(grep -r --files-with-matches --exclude-dir=.git --exclude-dir=.travis '#!.*/bin/.*sh' ./)"
-for I in $ALL_SHELL
-do
-    shellcheck "${I}"
-done
+bash testdata/shellcheck.bash
 
 echo "Shell checks passed."
 
