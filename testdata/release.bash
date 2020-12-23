6c14b51434c83209bb196195608f590ac0f970de
Jeremy Rand <jeremyrand@airmail.cc>
2020-12-23 21:11:41 +0000

    Cirrus: Add artifact upload

--- testdata/release.bash
+++ testdata/release.bash
@@ -27,14 +27,7 @@ if [[ "$GITHUB_TOKEN" == "" ]]; then
 fi
 
 # Make archives.
-echo Archiving releases...
-mkdir dist
-pushd idist || exit 1
-for x in *; do
-  echo "$x"
-  tar -caf "../dist/$(basename "$x").tar.gz" "$x"
-done
-popd || exit 1
+bash ./testdata/dist.bash
 
 pushd dist || exit 1
 
